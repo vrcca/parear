dbaf8a96c3b08455b03f416e7995908d31745493
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-09 23:53:42 -0200

    Saves pairing_statuses to database

---
+++ repository/test/repository/parear/pair_status_test.exs
@@ -0,0 +1,42 @@
+defmodule Repository.Parear.PairStatusTest do
+  import Ecto.Changeset
+  alias Repository.Parear.{PairStatus, Repo}
+  alias Parear.Stairs
+  use ExUnit.Case, async: true
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    %{pair_stairs: Parear.Stairs.new("Whiskey", limit: 10)}
+  end
+
+  test "Returns empty when there are no participants", %{pair_stairs: stairs} do
+    pair_statuses = PairStatus.convert_all_from(stairs)
+    assert Enum.empty?(pair_statuses)
+  end
+
+  test "Returns matching pairs with participants with zeroes", %{pair_stairs: stairs} do
+    stairs =
+      stairs
+      |> Stairs.add_participant("Vitor")
+      |> Stairs.add_participant("Kenya")
+
+    vitor = Stairs.find_participant_by_name(stairs, "Vitor")
+    kenya = Stairs.find_participant_by_name(stairs, "Kenya")
+
+    pair_statuses = PairStatus.convert_all_from(stairs)
+    assert pair_statuses |> length() == 2
+    assert pair_statuses |> has_matching_status?(vitor.id, kenya.id, 0)
+    assert pair_statuses |> has_matching_status?(kenya.id, vitor.id, 0)
+  end
+
+  defp has_matching_status?(statuses, participant_id, another_id, total) do
+    matches =
+      statuses
+      |> Enum.find(fn status ->
+        get_field(status, :participant_id) == participant_id &&
+          get_field(status, :friend_id) == another_id
+      end)
+
+    matches != nil && get_field(matches, :total) == total
+  end
+end
