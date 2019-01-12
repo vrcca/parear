ee69f0597913a6ddda0b8572be8a20832787e932
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-12 13:37:26 -0200

    Removes unnecessary methods from Repository

--- repository/lib/repository.ex
+++ repository/lib/repository.ex
@@ -1,4 +1,2 @@
 defmodule Repository do
-  defdelegate find_by_id(id), to: Repository.Stairs
-  defdelegate save(stairs), to: Repository.Stairs
 end
--- repository/lib/repository/stairs.ex
+++ repository/lib/repository/stairs.ex
@@ -1,24 +1,11 @@
-defmodule Repository.Stairs do
-  alias Repository.Parear.Stair
-
-  def find_by_id(id) do
-    Stair.find_by_id(id)
-  end
-
-  def save(stairs = %Parear.Stairs{}) do
-    Stair.save_cascade(stairs)
-    :ok
-  end
-end
-
 defimpl Parear.Repository, for: Parear.Stairs do
   def find_by_id(%Parear.Stairs{id: id}) do
-    stairs = Repository.Stairs.find_by_id(id)
+    stairs = Repository.Parear.Stair.find_by_id(id)
     {:ok, stairs}
   end
 
   def save(stairs = %Parear.Stairs{}) do
-    with :ok <- Repository.Stairs.save(stairs) do
+    with {:ok, result} <- Repository.Parear.Stair.save_cascade(stairs) do
       stairs
     end
   end
