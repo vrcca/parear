79e1b58b028b91d84df37e275ad7cde82798cf99
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-18 21:22:17 -0200

    Refactors variable to a more consistent name

--- parear/test/parear_test.exs
+++ parear/test/parear_test.exs
@@ -4,27 +4,27 @@ defmodule ParearTest do
 
   setup do
     start_supervised!(Parear.Support.MemoryRepository)
-    server = start_supervised!({Parear.Server, %{name: "Whiskey", options: [limit: 10]}})
-    %{server: server}
+    stairs = start_supervised!({Parear.Server, %{name: "Whiskey", options: [limit: 10]}})
+    %{stairs: stairs}
   end
 
-  test "Creates stairs", %{server: server} do
-    {:ok, stairs} = Parear.list(server)
+  test "Creates stairs", %{stairs: stairs} do
+    {:ok, stairs} = Parear.list(stairs)
     assert stairs.name == "Whiskey"
     assert stairs.limit == 10
     assert Enum.empty?(stairs.participants)
     assert not (stairs.id == "")
   end
 
-  test "Adds participants", %{server: server} do
-    Parear.add_participant(server, "Kenya")
-    {:ok, %{stairs: stairs}} = Parear.add_participant(server, "Vitor")
+  test "Adds participants", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Kenya")
+    {:ok, %{stairs: stairs}} = Parear.add_participant(stairs, "Vitor")
     assert true == Enum.any?(stairs, fn {participant, _} -> participant.name == "Vitor" end)
     assert true == Enum.any?(stairs, fn {participant, _} -> participant.name == "Kenya" end)
   end
 
-  test "Reloads from repository by id", %{server: server} do
-    {:ok, stairs} = Parear.list(server)
+  test "Reloads from repository by id", %{stairs: stairs} do
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
@@ -45,8 +45,8 @@ defmodule ParearTest do
     assert args == %{name: "unknown-name"}
   end
 
-  test "Reloads from repository by name", %{server: server} do
-    {:ok, stairs} = Parear.list(server)
+  test "Reloads from repository by name", %{stairs: stairs} do
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload_by_name(stairs.name)
@@ -55,9 +55,9 @@ defmodule ParearTest do
     assert stairs == reloaded_stairs
   end
 
-  test "Persists new participants to repository", %{server: server} do
-    Parear.add_participant(server, "Vitor")
-    {:ok, stairs} = Parear.list(server)
+  test "Persists new participants to repository", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Vitor")
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
@@ -66,10 +66,10 @@ defmodule ParearTest do
     assert stairs == reloaded_stairs
   end
 
-  test "Persists removed participants to repository", %{server: server} do
-    Parear.add_participant(server, "Vitor")
-    Parear.remove_participant(server, "Vitor")
-    {:ok, stairs} = Parear.list(server)
+  test "Persists removed participants to repository", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Vitor")
+    Parear.remove_participant(stairs, "Vitor")
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
@@ -78,11 +78,11 @@ defmodule ParearTest do
     assert stairs == reloaded_stairs
   end
 
-  test "Persists new pairings to repository", %{server: server} do
-    Parear.add_participant(server, "Vitor")
-    Parear.add_participant(server, "Kenya")
-    Parear.pair(server, "Vitor", "Kenya")
-    {:ok, stairs} = Parear.list(server)
+  test "Persists new pairings to repository", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Vitor")
+    Parear.add_participant(stairs, "Kenya")
+    Parear.pair(stairs, "Vitor", "Kenya")
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
@@ -91,12 +91,12 @@ defmodule ParearTest do
     assert stairs == reloaded_stairs
   end
 
-  test "Persists undone pairings to repository", %{server: server} do
-    Parear.add_participant(server, "Vitor")
-    Parear.add_participant(server, "Kenya")
-    Parear.pair(server, "Vitor", "Kenya")
-    Parear.unpair(server, "Vitor", "Kenya")
-    {:ok, stairs} = Parear.list(server)
+  test "Persists undone pairings to repository", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Vitor")
+    Parear.add_participant(stairs, "Kenya")
+    Parear.pair(stairs, "Vitor", "Kenya")
+    Parear.unpair(stairs, "Vitor", "Kenya")
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
@@ -105,12 +105,12 @@ defmodule ParearTest do
     assert stairs == reloaded_stairs
   end
 
-  test "Persists reseted counters to repository", %{server: server} do
-    Parear.add_participant(server, "Vitor")
-    Parear.add_participant(server, "Kenya")
-    Parear.pair(server, "Vitor", "Kenya")
-    Parear.reset_all_counters(server)
-    {:ok, stairs} = Parear.list(server)
+  test "Persists reseted counters to repository", %{stairs: stairs} do
+    Parear.add_participant(stairs, "Vitor")
+    Parear.add_participant(stairs, "Kenya")
+    Parear.pair(stairs, "Vitor", "Kenya")
+    Parear.reset_all_counters(stairs)
+    {:ok, stairs} = Parear.list(stairs)
 
     {:ok, reloaded_stairs} =
       Parear.reload(stairs.id)
