5b0515aeff990faee2246b6a4101669208289047
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-19 19:50:07 -0200

    Moves loading logic to its own module

--- parear/lib/parear.ex
+++ parear/lib/parear.ex
@@ -1,26 +1,17 @@
 defmodule Parear do
-  alias Parear.Stairs
-  alias Parear.Repository
+  alias Parear.Loader
 
   def new_stairs(name, options \\ []) do
-    with new_stairs <- Stairs.new(name, options),
-         {:ok, stairs_pid} <- new_stairs |> start_stairs(),
-         {:ok, _} <- stairs_pid |> save() do
-      stairs_pid
-      |> reply()
-    end
+    Loader.create(name, options)
   end
 
   def reload_by_id(id) do
-    %{id: id}
-    |> start_stairs()
-    |> reply()
+    Loader.load_by_id(id)
   end
 
+  # deprecated: for text_client only. to be removed once web is done
   def reload_by_name(name) do
-    Repository.find_by_name(%Stairs{name: name})
-    |> start_stairs()
-    |> reply()
+    Loader.load_by_name(name)
   end
 
   def add_participant(stairs, name) do
@@ -46,23 +37,4 @@ defmodule Parear do
   def list(stairs) do
     GenServer.call(stairs, {:list})
   end
-
-  def save(stairs) do
-    GenServer.call(stairs, {:save})
-  end
-
-  ## Helper methods
-  defp start_stairs({:ok, stairs = %Stairs{}}), do: start_stairs(stairs)
-  defp start_stairs({:none}), do: reply({:error, :stairs_could_not_be_found})
-  defp start_stairs(stairs) when is_pid(stairs), do: {:ok, stairs}
-
-  defp start_stairs(args) do
-    spec = {Parear.Server, args}
-    DynamicSupervisor.start_child(Parear.DynamicSupervisor, spec)
-  end
-
-  defp reply({:ok, pid}), do: reply(pid)
-  defp reply({:error, {:already_started, pid}}), do: reply(pid)
-  defp reply(pid) when is_pid(pid), do: pid
-  defp reply(error = {:error, _reason}), do: error
 end
