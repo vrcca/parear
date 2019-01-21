f1e0ddd4111b1039673191b4108acca707ec0521
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-21 21:54:27 -0200

    Displays pairing count

--- pair_stairs/test/pair_stairs_web/controllers/stairs_controller_test.exs
+++ pair_stairs/test/pair_stairs_web/controllers/stairs_controller_test.exs
@@ -13,9 +13,11 @@ defmodule PairStairsWeb.StairsControllerTest do
   end
 
   describe "show/2" do
-    test "displays stairs name", %{conn: conn} do
-      id = Parear.new_stairs("Whiskey")
+    setup do
+      %{stairs_id: Parear.new_stairs("Whiskey", limit: 100)}
+    end
 
+    test "displays stairs name", %{conn: conn, stairs_id: id} do
       response =
         conn
         |> get(Routes.stairs_path(conn, :show, id))
@@ -24,13 +26,10 @@ defmodule PairStairsWeb.StairsControllerTest do
       assert response =~ "Whiskey"
     end
 
-    test "displays participants", %{conn: conn} do
-      id = Parear.new_stairs("Whiskey")
-      participants = ["Vitor", "Kenya"]
-
-      for participant <- participants do
-        {:ok, _} = Parear.add_participant(id, participant)
-      end
+    test "displays participants", %{conn: conn, stairs_id: id} do
+      participants =
+        ["Vitor", "Kenya"]
+        |> add_participants(id)
 
       response =
         conn
@@ -42,7 +41,28 @@ defmodule PairStairsWeb.StairsControllerTest do
       end
     end
 
-    test "displays matching count" do
+    test "displays matching count", %{conn: conn, stairs_id: id} do
+      ["Vitor", "Kenya"]
+      |> add_participants(id)
+
+      for _i <- 1..37 do
+        Parear.pair(id, "Vitor", "Kenya")
+      end
+
+      response =
+        conn
+        |> get(Routes.stairs_path(conn, :show, id))
+        |> html_response(200)
+
+      assert response =~ "37"
     end
+  end
+
+  defp add_participants(participants, stairs_id) do
+    for participant <- participants do
+      {:ok, _} = Parear.add_participant(stairs_id, participant)
+    end
+
+    participants
   end
 end
