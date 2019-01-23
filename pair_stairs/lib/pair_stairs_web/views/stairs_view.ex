99b806a9cb67723f940c6bfd38f1189eca02d517
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-01-22 22:31:57 -0200

    WIP: Adds simplistic pairs stairs grid

--- pair_stairs/lib/pair_stairs_web/views/stairs_view.ex
+++ pair_stairs/lib/pair_stairs_web/views/stairs_view.ex
@@ -1,3 +1,8 @@
 defmodule PairStairsWeb.StairsView do
   use PairStairsWeb, :view
+  alias Parear.Stairs
+
+  def name_by_id(stairs = %Stairs{participants: participants}, id) do
+    participants[id].name
+  end
 end
