23bc74ded6d6c92f6f7332d956c440a5aee5fc1c
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-02-10 17:05:23 -0200

    Minor tweaks in the stairs and home page

--- pair_stairs/lib/pair_stairs_web/views/layout_view.ex
+++ pair_stairs/lib/pair_stairs_web/views/layout_view.ex
@@ -2,6 +2,18 @@ defmodule PairStairsWeb.LayoutView do
   use PairStairsWeb, :view
 
   def new_stairs_button(conn) do
-    link("New", to: Routes.stairs_path(conn, :new))
+    top_page_button(conn, conn.params)
+  end
+
+  defp top_page_button(conn, %{"id" => id}) do
+    top_page_button(conn, %{"stairs_id" => id})
+  end
+
+  defp top_page_button(conn, %{"stairs_id" => stairs_id}) do
+    link(gettext("Settings"), to: Routes.stairs_participant_path(conn, :index, stairs_id))
+  end
+
+  defp top_page_button(conn, _params) do
+    link(gettext("New"), to: Routes.stairs_path(conn, :new))
   end
 end
