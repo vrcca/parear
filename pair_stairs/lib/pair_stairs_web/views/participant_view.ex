a779be6de23032ef1e32bc5c954b3f42c51119a9
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-02-17 14:25:20 -0300

    Minor ui tweaks for better experience

--- pair_stairs/lib/pair_stairs_web/views/participant_view.ex
+++ pair_stairs/lib/pair_stairs_web/views/participant_view.ex
@@ -3,8 +3,7 @@ defmodule PairStairsWeb.ParticipantView do
 
   def return_to_stairs_button(conn) do
     %{"stairs_id" => stairs_id} = conn.path_params
-    {:ok, stairs} = stairs_id |> Parear.list()
-    link(stairs.name, to: Routes.stairs_path(conn, :show, stairs_id))
+    link(gettext("Return"), to: Routes.stairs_path(conn, :show, stairs_id))
   end
 
   def remove_participant_button(conn, participant_id) do
