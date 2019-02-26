93cfd28e9571b451b43fb118609b770ce107cea5
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-02-26 20:49:51 -0300

    use webpack for static assets instead of brunch

--- pair_stairs/config/prod.exs
+++ pair_stairs/config/prod.exs
@@ -10,9 +10,21 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pair_stairs, PairStairsWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+
+config :repository, Repository.Parear.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 2
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +80,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
