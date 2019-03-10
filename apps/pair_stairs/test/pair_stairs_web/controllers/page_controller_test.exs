92ae376e6cd69af36d044889dfde6765db0dfcb2
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-03-10 10:57:50 -0300

    Fix test

--- apps/pair_stairs/test/pair_stairs_web/controllers/page_controller_test.exs
+++ apps/pair_stairs/test/pair_stairs_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PairStairsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Parear"
+    assert html_response(conn, 200) =~ "Welcome to Pair Matrix"
   end
 end
